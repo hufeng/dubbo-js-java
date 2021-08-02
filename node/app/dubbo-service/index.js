556ccc2773b9ff769d155e667831499149243598
hufeng <fengmi99@gmail.com>
2021-08-02 16:18:47 +0800

    remove console.log

--- node/app/dubbo-service/index.js
+++ node/app/dubbo-service/index.js
@@ -4,8 +4,6 @@ const { DubboService, s } = require("apache-dubbo-service");
 const { Zk } = require("apache-dubbo-registry");
 const HelloService = require("./hello-service");
 
-console.log(s);
-
 module.exports = function startDubboService() {
   return new DubboService({
     registry: Zk({ connect: "localhost:2181" }),
